2c29ce9f37487910d2ac259672a02d3cb3c51f61
TexaThebardo <dinerillogamer@gmail.com>
2026-08-05 16:07:28 -0500

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -4,8 +4,8 @@
 
 // ⚠️ REEMPLAZA CON TUS DATOS DE SUPABASE
 const SUPABASE_CONFIG = {
-    url: 'https://TU_PROJECT_ID.supabase.co',
-    key: 'TU_ANON_PUBLIC_KEY'
+    url: 'https://azsbdlaaviuudgolswua.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6c2JkbGFhdml1dWRnb2xzd3VhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU5NTc3NDcsImV4cCI6MjEwMTUzMzc0N30.R2LTqDhbG368Ae-lOxyYWr09eZI1Dj1ARAvWAolrKX4'
 };
 
 // Inicializar Supabase
